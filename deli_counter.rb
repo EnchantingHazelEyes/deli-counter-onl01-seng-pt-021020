a484de5103d5cad2530091bd3eba910e90d52824
Samantha Barros <samantha.barros95@gmail.com>
2020-02-23 17:35:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ katz_deli = []
       other_deli = "The line is currently: "
       
        #katz_deli.each_with_index{|customer,number| other_deli << "#{number}. #{customer}"}
-     # puts "#{other_deli}"
+      puts "#{other_deli}"
     end
   end
   
