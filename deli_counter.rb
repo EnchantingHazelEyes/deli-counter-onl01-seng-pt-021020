f348b3aeb04d7c57b24dc3baa36537677d63d6c0
Samantha Barros <samantha.barros95@gmail.com>
2020-02-23 17:04:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 #katz_deli = []
   def line(other_deli)
     other_deli = []
-    if katz_deli.length < 1
+    if katz_deli.size < 1
       puts "The line is currently empty."
     else 
       katz_deli.each_with_index(1) {|customer,number| other_deli << "The line is currently: #{number}. #{customer}"}
