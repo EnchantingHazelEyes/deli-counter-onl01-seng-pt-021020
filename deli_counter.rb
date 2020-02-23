cca3134cfaed28252f56b4225c0ff4b543bd5674
Samantha Barros <samantha.barros95@gmail.com>
2020-02-23 17:24:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,7 @@ katz_deli = []
     if katz_deli.length == 0
       puts "The line is currently empty."
     else
-      puts "The line is currently: "
-    #if katz_deli.size >=1 
-      #katz_deli.each_with_index{|customer,number| other_deli << "#{number}. #{customer}"}
+       katz_deli.each_with_index{|customer,number| other_deli << "#{number}. #{customer}"}
       end
   end
   
