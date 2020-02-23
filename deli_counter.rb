45a9e793b5388af62d6789daaac2d39a600ad8df
Samantha Barros <samantha.barros95@gmail.com>
2020-02-23 17:30:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ katz_deli = []
     if katz_deli.length == 0
       puts "The line is currently empty."
     else
+      message = "The line is currently: "
+      
        katz_deli.each_with_index{|customer,number| other_deli << "#{number}. #{customer}"}
       end
   end
