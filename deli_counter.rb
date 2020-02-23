8db068186b6884014ad4e0362541a04a7a81e5aa
Samantha Barros <samantha.barros95@gmail.com>
2020-02-23 16:40:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+  def line(other_deli)
+    other_deli = []
+    if katz_deli.empty?
+      puts "The line is currently empty."
+    else 
+      katz_deli.each_with_index {|customer,number| other_deli << "The line is currently: #{number+1}. #{customer}"}
+      return other_deli
+    end
+  end
+    
